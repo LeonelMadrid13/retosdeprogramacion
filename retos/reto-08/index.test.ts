9f0a23ca646313c9bebe2cc84952257ad26df19c
LeonelMadrid13 <leoneleduardomadrid@hotmail.com>
2025-05-02 16:52:52 -0600

    feat: implement word counting functionality in solve function and update tests

--- retos/reto-08/index.test.ts
+++ retos/reto-08/index.test.ts
@@ -3,7 +3,12 @@ import { expect } from "jsr:@std/expect";
 import { solve } from "./index.ts";
 
 describe("Reto 08: CONTANDO PALABRAS", () => {
-  it(" ", () => {;
-    expect(solve()).toBe(0);
+  it(" ", () => {
+    const str = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    const solution = solve(str);
+    expect(solution["lorem"]).toBe(1);
+    expect(solution["ipsum"]).toBe(1);
+    expect(typeof solution).toBe(typeof {});
+    expect(solution["total"]).toBe(69);
   });
 });
--- retos/reto-08/index.ts
+++ retos/reto-08/index.ts
@@ -1,13 +1,32 @@
 // MEDIUM
 // Reto 08: CONTANDO PALABRAS
 
-export function solve(): number {
+export function solve(str: string): { [key: string]: number } {
   // TODO: implementar
-  return 0;
+  // 1. Eliminar signos de puntuación
+  // 2. Convertir a minúsculas
+  // 3. Separar las palabras
+  // 4. Contar las palabras
+  // 5. Mostrar el recuento final
+  // 6. Retornar el número de palabras
+  const punctuation = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+  const words = str.replace(punctuation, "").toLowerCase().split(/\s+/);
+  const wordCount: { [key: string]: number } = {};
+  for (const word of words) {
+    if (word) {
+      if (wordCount[word]) {
+        wordCount[word]++;
+      } else {
+        wordCount[word] = 1;
+      }
+    }
+  }
+  wordCount["total"] = words.length;
+  return wordCount
 }
 
 if (import.meta.main) {
-  solve();
+  console.log(solve("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
 }
 
 /*
