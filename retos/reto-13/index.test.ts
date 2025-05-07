ac37e804e91078ce5f6aa15e73ca79482e740025
LeonelMadrid13 <leoneleduardomadrid@hotmail.com>
2025-05-07 15:45:38 -0600

    feat: enhance palindrome solver with input sanitization and comprehensive test cases | reto 13

--- retos/reto-13/index.test.ts
+++ retos/reto-13/index.test.ts
@@ -3,7 +3,26 @@ import { expect } from "jsr:@std/expect";
 import { solve } from "./index.ts";
 
 describe("Reto 13: ¿ES UN PALÍNDROMO?", () => {
-  it(" ", () => {;
-    expect(solve()).toBe(0);
+  it("True palindromes", () => {;
+    /** add those as test cases
+      //true
+        console.log(solve("Ana lleva al oso la avellana."));
+        console.log(solve("Amo la pacífica paloma."));
+        console.log(solve("La ruta natural."));
+     */
+    expect(solve("Ana lleva al oso la avellana.")).toBe(true);
+    expect(solve("Amo la pacífica paloma.")).toBe(true);
+    expect(solve("La ruta natural.")).toBe(true);
+  });
+  it("False palindromes", () => {
+    /** add those as test cases
+      //false
+        console.log(solve("La casa no es un palíndromo."));
+        console.log(solve("La ruta no es natural."));
+        console.log(solve("La ruta no es un palíndromo."));
+     */
+    expect(solve("La casa no es un palíndromo.")).toBe(false);
+    expect(solve("La ruta no es natural.")).toBe(false);
+    expect(solve("La ruta no es un palíndromo.")).toBe(false);
   });
 });
--- retos/reto-13/index.ts
+++ retos/reto-13/index.ts
@@ -1,13 +1,37 @@
 // MEDIUM
 // Reto 13: ¿ES UN PALÍNDROMO?
 
-export function solve(): number {
+function limpiarTexto(texto: string): string {
+  return texto
+    .normalize('NFD')                         // Quita tildes
+    .replace(/[\u0300-\u036f]/g, '')          // Remueve diacríticos
+    .replace(/[^a-zA-Z0-9\s]/g, '')           // Remueve caracteres especiales
+    .replace(/\s+/g, '')                      // Opcional: colapsa múltiples espacios
+    .toLowerCase()                            // Convierte a minúsculas
+    .trim();                                  // Quita espacios al inicio y final
+}
+
+export function solve(str:string): boolean {
   // TODO: implementar
-  return 0;
+  // 1. Eliminar espacios, signos de puntuación y tildes
+  // 2. Convertir a minúsculas
+  // 3. Comparar la cadena original con la invertida
+  // 4. Retornar true o false
+  const sanitazedText = limpiarTexto(str);
+  return sanitazedText === sanitazedText.split('').reverse().join('');
 }
 
 if (import.meta.main) {
-  solve();
+  // true
+  console.log('True')
+  console.log(solve("Ana lleva al oso la avellana."));
+  console.log(solve("Amo la pacífica paloma."));
+  console.log(solve("La ruta natural."));
+  // false
+  console.log('False')
+  console.log(solve("La casa no es un palíndromo."));
+  console.log(solve("La ruta no es natural."));
+  console.log(solve("La ruta no es un palíndromo."));
 }
 
 /*
